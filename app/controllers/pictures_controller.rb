09dffdee36064d371e4a72b516f4c465555a811e
Yumiko <wakinghira73@gmail.com>
2021-05-10 14:49:43 +0900

    pictureのCRUD機能

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,72 @@
 class PicturesController < ApplicationController
+  before_acction :set_picture, only: [:show, :edit, :update, :destroy]
+
   def index
+    @pictures = Picture.all
+  end
+
+  def show
+  end
+
+  def new
+    @picture = Picture.new
+    if params[:back]
+      @picture = Picture.new(picture_params)
+    else
+      @picture = Picture.new
+    end
+  end
+
+  def confirm
+    @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id
+    render :new if @picture.invalid?
+  end
+
+  def edit
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id
+
+    respond_to do |format|
+      if @picture.save
+        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
+        format.json { render :show, status: :created, location: @picture }
+      else
+        format.html { render :new }
+        format.json { render :show, status: :created, location: @picture }
+      end
+     end
+  end
+
+  def update
+    respond_to do |format|
+      if @picture.update(picture_params)
+        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
+        format.json { render :show, status: :ok, location: @picture }
+      else
+        format.html { render :edit }
+        format.json { render json: @picture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @picture.destroy
+    respond_to do |format|
+      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def picture_params
+    params.require(:picture).paemit(:image, :image_cache, :content)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
